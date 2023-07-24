b71e7ba993b2bbe51b0eff2a173b08e1951cbaa3
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-07-25 00:19:18 +0600

    login and register ctrl added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model");
+const { createToken } = require("../helpers/token.helper");
 
 // Signup
 const createUser = async (req, res) => {
@@ -14,12 +15,28 @@ const createUser = async (req, res) => {
       occupation
     );
 
-    res.status(200).json(user);
+    const token = createToken(user._id);
+
+    res.status(200).json({ user, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
-const loginUser = async (req, res) => {};
+
+// Login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
   createUser,
